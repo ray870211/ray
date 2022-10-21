77d19d4aed46d2ed661e7b738f26dcdeb5b9bce5
ray <ray870211@gmail.com>
2022-10-21 08:34:00 +0800

    Update

---
+++ src/data/ArticleListOfPage.jsx
@@ -0,0 +1,26 @@
+export const ArticleData = [
+  {
+    id: 0,
+    postId: "4e06385d5b0546c59f97d91b07fc2802",
+    tag: [],
+    preview: "",
+  },
+  {
+    id: 1,
+    postId: "6e748f1da30e47b48b6b4fb48deb0c3d",
+    tag: [],
+    preview: "",
+  },
+  {
+    id: 2,
+    postId: "6e748f1da30e47b48b6b4fb48deb0c3d",
+    tag: [],
+    preview: "",
+  },
+  {
+    id: 3,
+    postId: "6e748f1da30e47b48b6b4fb48deb0c3d",
+    tag: [],
+    preview: "",
+  },
+];
