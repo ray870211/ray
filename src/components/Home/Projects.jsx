99657f7194f1ec29b5b070cc6d09d3e16fd68b4d
ray <ray870211@gmail.com>
2022-12-19 23:33:40 +0800

    update

--- src/components/Home/Projects.jsx
+++ src/components/Home/Projects.jsx
@@ -1,89 +1,47 @@
 import React, { useState } from "react";
 import Row from "react-bootstrap/esm/Row";
 import Col from "react-bootstrap/esm/Col";
-import { ProjectsData } from "../../data/ProjectsData";
 import "../../sass/components/projects.sass";
-import ProjectsModal from "./ProjectModal";
-
-function Projects() {
-  const [show, setShow] = useState(false);
-  const [project, setProject] = useState(0);
-  const expand = (project) => {
-    setShow(true);
-    setProject(project);
-  };
-
-  const AddProject = (data) => {
-    data = data.data;
-    // eslint-disable-next-line default-case
-    switch (data.template) {
-      case 1:
-        return (
-          <Row className='m-0 p-5'>
-            <Col
-              style={data.style.title}
-              className=' d-flex justify-content-center align-items-center'
-              xl={data.col[0]}
-              lg={data.col[0]}
-              md={data.col[0]}>
-              <h2>{data.title}</h2>
-              <img
-                className='add-icon'
-                onClick={() => expand(data.id)}
-                src={require("../../assets/icon/add.png")}></img>
-            </Col>
-            <Col xl={data.col[1]} lg={data.col[1]} md={data.col[1]}>
-              <img style={data.style.img} className='w-100' src={data.preview}></img>
-            </Col>
-          </Row>
-        );
-        break;
-      case 2:
-        return (
-          <Row className='m-0 p-5'>
-            <Col xl={data.col[1]} lg={data.col[1]} md={data.col[1]}>
-              <img style={data.style.img} className='w-100' src={data.preview}></img>
-            </Col>
-            <Col
-              style={data.style.title}
-              className='d-flex justify-content-center align-items-center'
-              xl={data.col[0]}
-              lg={data.col[0]}
-              md={data.col[0]}>
-              <h2>{data.title}</h2>
-              <img
-                className='add-icon'
-                onClick={() => expand(data.id)}
-                src={require("../../assets/icon/add.png")}></img>
-            </Col>
-          </Row>
-        );
-        break;
-      case 3:
-        return (
-          <Row className='m-3 p-2' style={data.style.title}>
-            <h3>{data.title}</h3>
-            {data.images.map((element) => (
-              <Col xl={data.col[1]} lg={data.col[1]} md={data.col[1]}>
-                <img style={data.style.img} className='w-100' src={element.url}></img>
-                <img
-                  className='add-icon'
-                  onClick={() => expand(data.id)}
-                  src={require("../../assets/icon/add.png")}></img>
-              </Col>
-            ))}
-          </Row>
-        );
-        break;
-    }
+import { motion } from "framer-motion";
+//icon
+import { ReactComponent as ArrowRight } from "../../assets/icon/arrow-right.svg";
+function Projects(props) {
+  const [isHovered, setHovered] = useState(false);
+  const projectData = props.projectData.projectData;
+  console.log("project");
+  const showProject = () => {
+    props.changeProjectState({ type: "modalShow", setShow: true });
+    props.changeProjectState({ type: "modalData", modalData: projectData });
   };
   return (
-    <div containerId='project' className='projects'>
-      {ProjectsData.map((element) => (
-        <AddProject key={element.id} data={element}></AddProject>
-      ))}
-      <ProjectsModal project={project} show={show} setShow={setShow}></ProjectsModal>
-    </div>
+    <React.Fragment>
+      <div
+        className='project'
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}>
+        <div className='project-image'>
+          <motion.div transition={{ duration: 0.4 }} animate={{ opacity: isHovered ? 1 : 0 }}>
+            <div className='bg-mask'></div>
+          </motion.div>
+          <img src={projectData.preview} alt='' />
+          <motion.div animate={{ opacity: isHovered ? 1 : 0 }}>
+            <div className='project-image-text'>
+              <div className='left'>
+                <div>WEBSIDE</div>
+                <div>{projectData.title}</div>
+              </div>
+              <div className='right'>
+                <div>
+                  <motion.div>
+                    <ArrowRight onClick={showProject} />
+                  </motion.div>
+                </div>
+              </div>
+            </div>
+          </motion.div>
+        </div>
+      </div>
+    </React.Fragment>
   );
 }
 export default Projects;
